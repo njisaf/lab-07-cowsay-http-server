8e33540ec03a7fbb9712dd110ba939348b8f6fdc
Nassir Isaf <njisaf@gmail.com>
2016-09-25 13:26:57 -0700

    Progress

---
+++ node-long-read/server.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const http = require('http');
+const url = require('url');
+
+function start(route, handle) {
+
+  function onRequest(req, res){
+    let pathname = url.parse(req.url).pathname;
+    console.log('Request for ' + pathname + ' received...');
+
+    route(handle, pathname);
+
+    res.writeHead(200, {'Content-type': 'text/plain'});
+    res.write('Hello World');
+    res.end();
+  }
+
+  http.createServer(onRequest).listen(8888);
+  console.log('Server has started.');
+}
+
+exports.start = start;
